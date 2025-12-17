d1c8fa0907558927a8e8cd539e72b5193e188946
Louis Van Steene <Louis.VanSteene@softwire.com>
2025-12-17 16:52:58 +0000

    beautify day3

--- day3/day3.rb
+++ day3/day3.rb
@@ -4,21 +4,20 @@ def read_banks
   File.readlines('input').map { |raw_bank| raw_bank.chomp.chars.map(&:to_i) }
 end
 
-def get_joltage(banks, k)
+def calculate_joltage(banks, k)
   banks.sum do |bank|
-    # max between indexOfLast+1 and size-digitsRemaining (inclusive)
-    index_of_last = 0
+    index_of_last_max = 0
     k.downto(1).map do |digits_remaining|
-      splice = bank[index_of_last..bank.size - digits_remaining]
+      splice = bank[index_of_last_max..bank.size - digits_remaining]
       max = splice.max
 
-      index_of_last += splice.index(max) + 1
+      index_of_last_max += splice.index(max) + 1
 
       max
     end.join('').to_i
   end
 end
 
-puts "Part 1: #{get_joltage(read_banks, 2)}"
+puts "Part 1: #{calculate_joltage(read_banks, 2)}"
 
-puts "Part 2: #{get_joltage(read_banks, 12)}"
+puts "Part 2: #{calculate_joltage(read_banks, 12)}"
